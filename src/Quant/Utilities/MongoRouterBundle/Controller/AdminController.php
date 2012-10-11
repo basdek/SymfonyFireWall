bf958b1b0bc33808fb0899380b662ffb45ed1612
Quant <basdekwant@gmail.com>
2012-10-12 01:23:05 +0200

    Extended. Error with form not yet fixed.

--- src/Quant/Utilities/MongoRouterBundle/Controller/AdminController.php
+++ src/Quant/Utilities/MongoRouterBundle/Controller/AdminController.php
@@ -8,47 +8,25 @@ use Quant\Utilities\MongoRouterBundle\Form\Type\RouteType;
 
 class AdminController extends Controller
 {
-    /* 
-     * @QUANT
-     * @NOTE: temporary action to fill databaseclear
-     */
-
-//    public function qInsAction()
-//    {
-//        $em = $this->get('doctrine_mongodb')->getManager();
-//
-//        for ($i = 10; $i < 11; $i++)
-//        {
-//            $r = new Route();
-//
-//            $r->setDestinationController('MongoRouterBundle/AdminController');
-//            $r->setDestinationAction('index');
-//            $r->setPattern('/rtest' . $i);
-//            $r->setPriority($i);
-//            $r->setPostRequired(true);
-//            $r->setActive(false);
-//            $em->persist($r);
-//            $em->flush();
-//        }
-//    }
 
     public function indexAction()
     {
         $em = $this->get('doctrine_mongodb')->getManager();
         $routes = $em->getRepository('QuantUtilitiesMongoRouterBundle:Route')->findAll();
-        $test;
     }
+
     public function addRouteAction()
     {
         $form = $this->createForm(new RouteType, new Route);
-        
         return $this->render('QuantUtilitiesMongoRouterBundle:Admin:add.html.twig', array('form' => $form->createView()));
     }
+
     public function createRouteAction()
     {
         
     }
-    public function deleteRouteAction()
+
+    public function deleteRouteAction($id)
     {
         
     }

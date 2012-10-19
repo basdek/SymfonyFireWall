2664e5ef0b85b5b7e96dcb603b9f9975442e758a
Quant <basdekwant@gmail.com>
2012-10-20 00:07:02 +0200

    I don't know if this is any good.

--- src/Quant/Utilities/MongoRouterBundle/Controller/ResolveController.php
+++ src/Quant/Utilities/MongoRouterBundle/Controller/ResolveController.php
@@ -10,20 +10,28 @@ class ResolveController extends Controller
 
     protected $input_pattern;
     protected $available_database_routes;
+
     public function resolveAction($path)
     {
-        $this->input_pattern = explode('/', $path);
         $em = $this->get('doctrine_mongodb')->getManager();
-        
         $q = $em->createQueryBuilder('QuantUtilitiesMongoRouterBundle:Route');
         $q->field('active')->notEqual('NO');
         $q->sort('priority', 'ASC');
+
         $this->available_database_routes = $q->getQuery()->execute();
-        
-        /*
-         * Deny requests that are not yet resolvable because the component is not yet finished.
-         */
-        return new \Symfony\Component\HttpFoundation\Response('QuantRouter temporary denied request.', 404);
+
+        foreach ($this->available_database_routes as $r)
+        {
+            preg_match_all('#.\{(\w+)\}#', $r->getPattern(), $matches, PREG_OFFSET_CAPTURE | PREG_SET_ORDER);
+            var_dump($matches); 
+            if (false === strpos($r->getPatternSmart(), $path))
+            {
+                continue;
+            } else
+            {
+                print 'Match';
+            }
+        }
     }
 
     private function parseMatchUrl()
